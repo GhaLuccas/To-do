8997485f084e8958a86f19f234e307f471928118
ghaluccas <gabsgit@gmail.com>
2024-12-03 11:48:23 -0300

    Task model created , migrations were made

--- Todo/task/models.py
+++ Todo/task/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=250)
+    context = models.TextField(max_length=500)
+    author = models.ForeignKey(User , on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    done = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
