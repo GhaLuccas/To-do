4d863f2f695d98e2b1d58af32dfe907c89bcead2
GhaLuccas <gitsgab@gmail.com>
2024-12-03 20:01:56 -0300

    creating the form view and html

--- Todo/task/urls.py
+++ Todo/task/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page , task_page , TaskCreateView
 
 app_name = 'task'
 
 urlpatterns =[
     path("home/" , home_page , name='home'),
+    path('task/' , task_page.as_view() , name='task') ,
+    path('task/create' , TaskCreateView.as_view() , name='create-task' )
 ]
--- Todo/task/views.py
+++ Todo/task/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.views import generic , View  
+from django.views.generic import CreateView , TemplateView
+from django.views.generic.edit import FormView
+from .models import Task
+from . forms import TaskForm
 
 # Create your views here.
 
 def home_page(request):
     return render(request , 'home.html')
 
+class task_page(TemplateView):
+    template_name = 'task.html'
 
+
+
+class TaskCreateView(FormView):
+    template_name = 'create_task.html'
+    form_class = TaskForm
+    success_url = '//'
+    
+    def form_valid(self, form):
+        return HttpResponseRedirect(self.get_success_url()) 
