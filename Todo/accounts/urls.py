4e577f31c6aab52e2a2bec7c355dd19560637a1a
GhaLuccas <gitsgab@gmail.com>
2024-12-07 15:16:26 -0300

    chanege e home to beter now if login or not

--- Todo/accounts/urls.py
+++ Todo/accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path 
-from .views import UserCreateView , LoginView
-
+from .views import UserCreateView , LoginView , LogoutView
 app_name='accounts'
 
 urlpatterns = [
     path('register/', UserCreateView.as_view() , name='register' ),
     path('login/', LoginView.as_view() , name='login' ),
+    path('logout/',  LogoutView.as_view() , name='logout'),
 ]
