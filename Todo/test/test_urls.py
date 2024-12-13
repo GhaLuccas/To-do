01459d013787f7e4bcb9c4813f7dbb41543cbbcb
ghaluccas <gabsgit@gmail.com>
2024-12-13 16:00:31 -0300

    colors update

--- Todo/test/test_urls.py
+++ Todo/test/test_urls.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase , Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from task import views
@@ -9,8 +9,11 @@ class TestUrl(TestCase):
     
     def setUp(self):
         self.user = User.objects.create_superuser(username='testuser',password='test123')
+        self.client = Client()
+        self.valid_data = {'title':"hello" , 'context':"word"}
         self.home_url = reverse('task:home')
         self.task_url = reverse('task:task')
+
         
     def test_home_url(self):
         home_response = self.client.get(self.home_url)
@@ -22,6 +25,9 @@ class TestUrl(TestCase):
         task_response = self.client.get(self.task_url)
         self.assertEqual(task_response.status_code ,status.HTTP_200_OK )
         self.assertTemplateUsed(task_response , 'task.html')
-        
+    
+    def test_task_create(self):
+        self.client.login(username='testuser' , password='test123')
+
 
 
